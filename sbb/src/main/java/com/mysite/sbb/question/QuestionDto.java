d30ddb2d0ab7c74af56d614eb3dbb4588ec24f0a
이성현 <marvel97@naver.com>
2022-07-04 13:48:00 +0900

    Question Dto

--- sbb/src/main/java/com/mysite/sbb/question/QuestionDto.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionDto.java
@@ -1,24 +1,20 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.answer.AnswerDto;
+import com.mysite.sbb.user.SiteUserDto;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
+import lombok.Data;
 
-import com.mysite.sbb.answer.AnswerDto;
-import com.mysite.sbb.user.SiteUserDto;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+@Data
 public class QuestionDto {
-    private Integer id;
-    private String subject;
-    private String content;
-    private LocalDateTime createDate;
-    private List<AnswerDto> answerList;
-    private SiteUserDto author;
-    private LocalDateTime modifyDate;
-    private Set<SiteUserDto> voter;
+  private Integer id;
+  private String subject;
+  private String content;
+  private LocalDateTime createDate;
+  private List<AnswerDto> answerList;
+  private SiteUserDto author;
+  private LocalDateTime modifyDate;
+  private Set<SiteUserDto> voter;
 }
