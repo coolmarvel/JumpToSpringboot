5d9a5ce3010fa0d3b5fffdfbdf1de266434aaeef
이성현 <marvel97@naver.com>
2022-07-04 13:24:49 +0900

    User Role

--- sbb/src/main/java/com/mysite/sbb/user/UserRole.java
+++ sbb/src/main/java/com/mysite/sbb/user/UserRole.java
@@ -2,15 +2,14 @@ package com.mysite.sbb.user;
 
 import lombok.Getter;
 
-
 @Getter
 public enum UserRole {
-    ADMIN("ROLE_ADMIN"),
-    USER("ROLE_USER");
+  ADMIN("ROLE_ADMIN"),
+  USER("ROLE_USER");
 
-    UserRole(String value) {
-        this.value = value;
-    }
+  UserRole(String value) {
+    this.value = value;
+  }
 
-    private String value;
+  private String value;
 }
