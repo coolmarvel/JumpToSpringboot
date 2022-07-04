cfde51a118caa04c3ecf9e8fe45609513d4a2a39
이성현 <marvel97@naver.com>
2022-07-04 13:49:53 +0900

    AnswerRepository

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerRepository.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerRepository.java
@@ -2,6 +2,4 @@ package com.mysite.sbb.answer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-
-}
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {}
