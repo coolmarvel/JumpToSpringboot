82d5361430013679466f0faa6c3ae7a3e9d89e05
이성현 <marvel97@naver.com>
2022-07-04 13:49:25 +0900

    Answer Entity

--- sbb/src/main/java/com/mysite/sbb/answer/Answer.java
+++ sbb/src/main/java/com/mysite/sbb/answer/Answer.java
@@ -1,8 +1,9 @@
 package com.mysite.sbb.answer;
 
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
 import java.time.LocalDateTime;
 import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,34 +11,28 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import lombok.Data;
 
-import com.mysite.sbb.question.Question;
-import com.mysite.sbb.user.SiteUser;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+@Data
 @Entity
 public class Answer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
 
-    @Column(columnDefinition = "TEXT")
-    private String content;
+  @Column(columnDefinition = "TEXT")
+  private String content;
 
-    private LocalDateTime createDate;
+  private LocalDateTime createDate;
 
-    @ManyToOne
-    private Question question;
-    
-    @ManyToOne
-    private SiteUser author;
-    
-    private LocalDateTime modifyDate;
-    
-    @ManyToMany
-    Set<SiteUser> voter;
+  @ManyToOne
+  private Question question;
+
+  @ManyToOne
+  private SiteUser author;
+
+  private LocalDateTime modifyDate;
+
+  @ManyToMany
+  Set<SiteUser> voter;
 }
