4959a8a85ab1777038750f42d41ae15cc4e4c246
이성현 <marvel97@naver.com>
2022-07-04 13:41:49 +0900

    Lombok Practice

--- sbb/src/main/java/com/mysite/sbb/HelloLombok.java
+++ sbb/src/main/java/com/mysite/sbb/HelloLombok.java
@@ -4,17 +4,17 @@ import lombok.Data;
 
 @Data
 public class HelloLombok {
-    private final String hello;
-    private final int lombok;
+  private final String hello;
+  private final int lombok;
 
-    public HelloLombok(String hello, int lombok) {
-        this.hello = hello;
-        this.lombok = lombok;
-    }
+  public HelloLombok(String hello, int lombok) {
+    this.hello = hello;
+    this.lombok = lombok;
+  }
 
-    public static void main(String[] args) {
-        HelloLombok helloLombok = new HelloLombok("헬로", 5);
-        System.out.println(helloLombok.getHello());
-        System.out.println(helloLombok.getLombok());
-    }
+  public static void main(String[] args) {
+    HelloLombok helloLombok = new HelloLombok("헬로", 5);
+    System.out.println(helloLombok.getHello());
+    System.out.println(helloLombok.getLombok());
+  }
 }
